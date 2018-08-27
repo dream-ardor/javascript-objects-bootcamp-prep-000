9c4203db5e4b05c2e8cfe38fde1a8f4a9b0d60a3
Dan Barrick <barrick.dan80@gmail.com>
2018-08-27 00:54:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,16 @@
-var playlist = { artist : "End Game"};
+var playlist = {
+  artist1: "song1",
+  artist2: "song2",
+  artist3: "song3"
+};
 
-function updatePlaylist(object,key,value) {
-   return Object.assign({}, object, { [key]: value})
+function updatePlaylist(thePlaylist, artistName, songTitle) {
+  return Object.assign({}, thePlaylist, { [artistName]: songTitle});
 }
 
-function removeFromPlaylist(playlist, artistName){
-  delete playlist[artistName]
-  return playlist
+updatePlaylist(playlist, "artist4", "song4");
+
+function removeFromPlaylist(thePlaylist, artistName) {
+  delete thePlaylist[artistName];
+  return thePlaylist;
 }
